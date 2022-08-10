37113aaad3b7098c50217a4ac4f4d07007164d1a
CzechCoder <92126675+CzechCoder@users.noreply.github.com>
2022-08-10 07:58:53 +0200

    Delete src directory

--- src/state/actions/userAction.tsx
+++
@@ -1,8 +0,0 @@
-import { AnyAction, ThunkAction, ThunkDispatch } from "@reduxjs/toolkit";
-import { RootState } from "../store";
-import { USER_LOGIN_SUCCESS } from "../../constants/userConstants";
-
-// logging the user in
-export const login = (email: string, password: string) => async (dispatch: any) => {
-  console.log(`${email}, ${password}`);
-};
